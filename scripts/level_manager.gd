84f9f6cea5ffce68a562dcdf5e46a8273e12563d
Narik-tech <kiranmeyers@gmail.com>
2026-01-31 20:14:02 -0500

    adjust animation, fix bug for exported levels

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -1,26 +1,34 @@
 extends Node
 
-var levels := []
-var level_folder = "res://levels"
-var current_level_index := -1
+@export var levels := [
+	"res://levels/level_1.tscn",
+	"res://levels/level_2.tscn",
+	"res://levels/level_3.tscn",
+]
+#var level_folder = "res://levels"
+var current_level_index := 0
 
-func _ready():
-	# Load all level scene paths from res://levels
-	var dir := DirAccess.open(level_folder)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if not dir.current_is_dir() and file_name.ends_with(".tscn"):
-				levels.append(level_folder + "/" + file_name)
-			file_name = dir.get_next()
-		dir.list_dir_end()
-
-	levels.sort()
-
-	# Detect current level
-	var current_scene_path := get_tree().current_scene.scene_file_path
-	current_level_index = levels.find(current_scene_path)
+#func _ready():
+	## Load all level scene paths from res://levels
+	#var dir := DirAccess.open(level_folder)
+	#if dir:
+		#dir.list_dir_begin()
+		#var file_name = dir.get_next()
+		#while file_name != "":
+			#print_debug("file found: " + file_name)
+			#if not dir.current_is_dir() and file_name.ends_with(".tscn"):
+				#print_debug("level found: " + level_folder + "/" + file_name)
+				#levels.append(level_folder + "/" + file_name)
+			#file_name = dir.get_next()
+		#dir.list_dir_end()
+	#else:
+		#print_debug("could not open dir")
+#
+	#levels.sort()
+#
+	## Detect current level
+	#var current_scene_path := get_tree().current_scene.scene_file_path
+	#current_level_index = levels.find(current_scene_path)
 
 func level_complete():
 	var next_index := current_level_index + 1
